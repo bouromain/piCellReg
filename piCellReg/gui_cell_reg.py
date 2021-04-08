a2645f857ad2f907aa136036869a8b2e34f2be46
bouromain <bouromain@gmail.com>
2021-04-08 20:54:17 +0100

    small progress

--- piCellReg/gui_cell_reg.py
+++ piCellReg/gui_cell_reg.py
@@ -3,31 +3,63 @@ import pyqtgraph as pg
 import sys
 from PyQt5 import QtGui, QtCore
 
+
 class MainWindow(QtGui.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.setWindowTitle("yo mama")
-        self.setGeometry(100,100,1500,800)
+        self.setWindowTitle("load data or process")
+        self.setGeometry(100, 100, 1500, 800)
 
         # Layout
+        cwidget = QtGui.QWidget()
+        self.l0 = QtGui.QGridLayout()
+        cwidget.setLayout(self.l0)
+        self.setCentralWidget(cwidget)
 
-        self.graphWidget = pg.PlotWidget()
-        self.setCentralWidget(self.graphWidget)
+        # populate layout
+        self.make_list()
+        self.make_images()
 
-        hour = [1,2,3,4,5,6,7,8,9,10]
-        temperature = [30,32,34,32,33,31,29,32,35,45]
+    def make_list(self):
+        self.list_left = ListSession()
+        self.list_left.insertItem(0, "Red")
+        self.list_left.insertItem(1, "blue")
+        self.list_left.insertItem(2, "bulue")
+        self.l0.addWidget(self.list_left, 0, 0, 1, 2)
 
-        self.graphWidget.plot(hour, temperature)
+        self.list_right = ListSession()
+        self.list_right.insertItem(0, "Red2")
+        self.list_right.insertItem(1, "blue")
+        self.list_right.insertItem(2, "green")
+        self.list_right.itemClicked.connect(self.list_right.clicked)
 
-        # show stuff
-        self.show()
+        self.l0.addWidget(self.list_right, 0, 1, 1, 2)
+
+    def make_images(self):
+        p = "/home/bouromain/Sync/tmpData/crossReg/4453/20201217/ops.npy"
+        ops1 = np.load(p, allow_pickle=True)
+        ops1 = ops1.item()
+
+        self.win = pg.GraphicsLayoutWidget()
+        self.l0.addWidget(self.win, 0, 1)
+        self.viewIm1 = pg.ViewBox()
+        self.win.addItem(self.viewIm1, 0, 1)
+
+        self.im1 = pg.ImageItem(ops1["meanImgE"])
+        self.viewIm1.addItem(self.im1)
+
+
+class ListSession(QtGui.QListWidget):
+    def clicked(self, item):
+        print("yo")
 
 
 def main():
     app = QtGui.QApplication(sys.argv)
     GUI = MainWindow()
+    GUI.show()
     sys.exit(app.exec())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
