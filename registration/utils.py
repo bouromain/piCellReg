cccf0517dd3ba22df861e9ac0b75f7a93b5ec4c3
Romain <bouromain@gmail.com>
2021-05-15 09:07:09 +0100

    progress on project to polar

--- registration/utils.py
+++ registration/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 import bottleneck as bn
+from scipy.ndimage import map_coordinates
 
 
 def gaussian_2D(Ly, Lx, sigma=None):
@@ -29,21 +30,60 @@ def gaussian_2D(Ly, Lx, sigma=None):
     return G
 
 
-def project_to_polar(im, theta_bin):
+def project_to_polar(im):
     """
-    ...
+    Parameters:
+        - image
+
+    Return:
+        - projection of the image in polar coordinates
+
+
+    Adapted from:
+    https://stackoverflow.com/questions/3798333/image-information-along-a-polar-coordinate-system
+    https://github.com/PyAbel/PyAbel/blob/master/abel/tools/polar.py
+
+    TO DO:
+    Check if image is a 3D stack and loop over it
+    check if we can remove some steps with the r and theta min/max
     """
-    Ly, Lx = im.shape
+
+    Ly, Lx = im.shape[:2]
     origin_x = Lx // 2
     origin_y = Ly // 2
 
+    # determine the min and max r and theta coordinates
+    # First, calculate cartesian coordinates
     xx = np.arange(Lx)
     yy = np.arange(Ly)
 
+    # center them
     xx -= origin_x
     yy -= origin_y
 
+    # convert to polar
     r, theta = cartesian_to_polar(xx, yy)
+
+    n_bin_r = int(np.ceil(r.max() - r.min()))
+    n_bin_t = int(np.ceil(r.max() - r.min()))
+
+    # create grid coordinate in polar space
+    r_idx = np.linspace(r.min(), r.max(), n_bin_r, endpoint=False)
+    theta_idx = np.linspace(theta.min(), theta.max(), n_bin_t, endpoint=False)
+    theta_grid, r_grid = np.meshgrid(theta_idx, r_idx)
+
+    # re-project the polar grid in pixel coordinates (cartesian)
+    X, Y = polar_to_cartesian(r_grid, theta_grid)
+    X += origin_x
+    Y += origin_y
+
+    # prepare thing for map coordinates
+    coord = np.vstack((X.flatten(), Y.flatten()))
+
+    Z = map_coordinates(im, coord, output=float)
+    Z = np.reshape(Z, (n_bin_r, n_bin_t))
+
+    return Z, r_grid, theta_grid
 
 
 def cartesian_to_polar(x, y):
